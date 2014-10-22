951d0a30bd553e96521f32bbf86deacc0abac571
paulamelo <paulagsmelo@outlook.com>
2014-10-22 11:23:18 -0200

    Disciplinas

---
+++ ProjetoGUASCA/Guasca/src/java/guasca/dao/DisciplinaDao.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guasca.dao;
+
+import guasca.modelo.Disciplina;
+import guasca.util.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Paula
+ */
+public class DisciplinaDao implements DisciplinaInterface{
+ 
+    @Override
+    public void cadastrarDisciplina(Disciplina nova) throws Exception {
+        Connection conexao = null;
+        PreparedStatement ps = null;
+
+        try {
+
+            conexao = Conexao.abrirConexao();
+            ps = conexao.prepareStatement("insert into disciplina (nome) values (?)");
+
+            ps.setString(1, nova.getDescricao());
+            ps.execute();
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+            throw new Exception(e.getMessage());
+        } finally {
+            ps.close();
+            conexao.close();
+        }
+    }
+
+    @Override
+    public void atualizarDisciplina(int idDisciplina) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void deletarDisciplina(int idDisciplina) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Disciplina> buscarDisciplinas() throws Exception {
+        List<Disciplina> lista = new ArrayList<Disciplina>();
+
+        Connection conexao   = null;
+        PreparedStatement ps = null;
+        ResultSet rs         = null;
+
+        try {
+            
+            conexao = Conexao.abrirConexao();
+            ps = conexao.prepareStatement("select * from disciplina");
+            rs = ps.executeQuery();
+            
+            Disciplina disciplina;
+            
+            while(rs.next()){
+                disciplina = new Disciplina(
+                          rs.getInt("id_disciplina")
+                        , rs.getString("nome"));
+                lista.add(disciplina);
+            }
+            
+            
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+            throw new Exception(e.getMessage());
+        } finally {
+            ps.close();
+            conexao.close();
+        }
+
+        return lista;
+    }
+}
---
+++ ProjetoGUASCA/Guasca/src/java/guasca/dao/DisciplinaInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guasca.dao;
+
+import guasca.modelo.Disciplina;
+import java.util.List;
+
+/**
+ *
+ * @author Paula
+ */
+public interface DisciplinaInterface {
+    public abstract void cadastrarDisciplina(Disciplina nova) throws Exception;
+    public abstract void atualizarDisciplina(int idDisciplina) throws Exception;
+    public abstract void deletarDisciplina(int idDisciplina) throws Exception;
+    public abstract List<Disciplina> buscarDisciplinas() throws Exception;
+}
